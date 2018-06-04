58dee5abfe7dc9dc133584cd1b40c75e7015cdc8
nyan <1eaegue@gmail.com>
2018-06-04 23:20:18 +0300

    implement comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
+  has_one_attached :avatar
   belongs_to :article
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :tags,        path: 'admin/tags'
   resources :categories,  path: 'admin/categories'
   resources :articles,    path: 'admin/articles'
+  resources :comments,    path: 'admin/comments'
   root to: 'pages#home'
   get '/c/:slug', to: 'pages#category'
   get '/:slug', to: 'pages#article'
