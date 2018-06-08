1fc7e25c5c9bc0c1da297c39c4421155e91af9a9
nyan <1eaegue@gmail.com>
2018-06-08 22:12:56 +0300

    -

--- db/migrate/20180523105303_add_default_to_subjects.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultToSubjects < ActiveRecord::Migration[5.2]
-  def change
-    change_column :subjects, :created_at, :datetime, default: -> { 'current_timestamp' }
-    change_column :subjects, :updated_at, :datetime, default: -> { 'current_timestamp' }
-  end
-end
