6696f3be475a9a7770c652dfa8322fdc297b48b7
nyan <1eaegue@gmail.com>
2018-06-18 13:57:27 +0300

    t

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,8 +1,7 @@
 Rails.application.configure do
-
-  config.active_storage.service = :test
-
   # Settings specified here will take precedence over those in config/application.rb.
+
+  config.active_storage.service = :local
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
