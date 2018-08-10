0ca47c9ce0833e823a99ceb6772af1efa4e01547
nyan <1eaegue@gmail.com>
2018-08-10 12:38:10 +0300

    fix breadcrumbs linking

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :tags,        path: 'admin/tags'
-  resources :categories,  path: 'admin/categories'
-  resources :articles,    path: 'admin/articles'
-  resources :comments,    path: 'admin/comments'
-  root to: 'pages#home'
-  get '/c/:slug', to: 'pages#category'
-  get '/:slug', to: 'pages#article'
+  resources :tags,        path: "admin/tags"
+  resources :categories,  path: "admin/categories"
+  resources :articles,    path: "admin/articles"
+  resources :comments,    path: "admin/comments"
+  root to: "pages#home"
+  get "/c/:slug", to: "pages#category", as: :category_page
+  get "/:slug", to: "pages#article"
 end
