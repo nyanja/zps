dfa470000c9f083ed9acaec053df36009ed37719
nyan <1eaegue@gmail.com>
2018-08-25 13:31:17 +0300

    md hint

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,5 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-function updateArticleContentHint(value = $("#article-content").text()) {
-  $("#article-content-hint").text( value.length + " | " + value.replace(/[^A-zА-я]/g, "").length)
+function updateInputCounter(id, value = null) {
+  if (!value) {
+    value = $("#" + id).text()
+  }
+  $("#" + id + "-hint").text( value.length + " | " + value.replace(/[^A-zА-я]/g, "").length)
 }
